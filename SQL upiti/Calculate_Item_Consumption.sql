06ae3b352ae25d185a9b54d8fe5b9b94ec8a18e7
nevena94rad <nevenarad1994@gmail.com>
2017-12-22 14:36:16 +0100

    reverse

--- SQL upiti/Calculate_Item_Consumption.sql
+++ SQL upiti/Calculate_Item_Consumption.sql
@@ -4,7 +4,7 @@ SET @StartDT =  '20150500'
 WHILE @StartDT < '20171200'
 BEGIN
 	INSERT INTO PED.dbo.ItemConsumption (ItemNo, Date, Consumption)
-	SELECT a.ItemNo,@StartDT,(cast (SUM(PurchasePeriod) as float)/SUM(InvQty)) as prosPotrosnja FROM
+	SELECT a.ItemNo,@StartDT,(cast (SUM(InvQty) as float)/SUM(PurchasePeriod)) as prosPotrosnja FROM
 		Ped.dbo.PurchasePeriods a INNER JOIN PED.dbo.PurchaseHistory b
 		ON a.ItemNo = b.ItemNo AND a.CustNo= b.CustNo AND a.InvDatePrior = CONVERT(datetime, convert(varchar(10), b.InvDate))
 		WHERE CONVERT(datetime, convert(varchar(10), @StartDT)) <= a.InvDateCurr AND CONVERT(datetime, convert(varchar(10), @StartDT)) >= a.InvDatePrior
--- SQL upiti/CustItemDailyConsumption.sql
+++ SQL upiti/CustItemDailyConsumption.sql
@@ -1,14 +1,14 @@
 DECLARE @StartDT INT
-SET @StartDT =  '20160123'
+SET @StartDT =  '20150816'
 
-WHILE @StartDT < '20171200'
+WHILE @StartDT < '20171005'
 BEGIN
 	INSERT INTO PED.dbo.CustItemDailyCons (ItemNo,CustNo,ProcessingDate,AvgConsumption)
-	SELECT a.ItemNo,a.CustNo,@StartDT, CAST(PurchasePeriod AS FLOAT)/(InvQty)  as prosPotrosnja FROM
+	SELECT a.ItemNo,a.CustNo,@StartDT, CAST(InvQty AS FLOAT)/(PurchasePeriod)  as prosPotrosnja FROM
 		PED.dbo.PurchasePeriods a INNER JOIN PED.dbo.PurchaseHistory b
 		ON a.ItemNo = b.ItemNo AND a.CustNo= b.CustNo AND a.InvDatePrior = CONVERT(datetime, convert(varchar(10), b.InvDate))
 		WHERE CONVERT(datetime, convert(varchar(10), @StartDT)) <= a.InvDateCurr AND 
 		CONVERT(datetime, convert(varchar(10), @StartDT)) >= a.InvDatePrior
-				AND a.ItemNo = '10-4784' AND a.CustNo = 'NET121'
+				AND a.ItemNo = '10-4784' AND a.CustNo = 'FIV108'
 		set @StartDT=@StartDT+1
 END
