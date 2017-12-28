cc8b870d8d575564ce0f9c659bfc438b3ce9b45d
AlexStojcic94 <alexstojcic@elfak.rs>
2017-12-28 10:46:16 +0100

    VS NN

---
+++ NN/Baza/DTO/LearningData.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza.DTO
+{
+    public class LearningData
+    {
+        public List<SinglePointOfData> allData;
+
+        private static LearningData _instance;
+
+        public LearningData Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new LearningData();
+
+
+                return _instance;
+            }
+        }
+
+        private LearningData()
+        {
+            allData = new List<SinglePointOfData>();
+        }
+    }
+}
