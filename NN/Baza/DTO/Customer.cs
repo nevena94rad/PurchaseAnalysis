721333d6c471f57451d9d0d939352f87cbc56238
AlexStojcic94 <alexstojcic@elfak.rs>
2017-12-28 11:32:05 +0100

    Ostavljen prostor za popunjavanje funkcija

---
+++ NN/Baza/DTO/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza.DTO
+{
+    public class Customer
+    {
+        public List<string> itemNos;
+        public string custNo;
+
+        public void getAllItems()
+        {
+            // ucitati sve iteme koje je cust narucio vise od 2 puta (3+) 
+            throw new Exception();
+        }
+        public List<Prediction> makeAllPredictions()
+        {
+            // za svaki item se prave predikcije
+            // ako item ima n kupovina od strane customera (n>=3)
+            // predikcije se prave na osnovu k datuma gde je ( 2<=k<n)
+            // za svaku predikciju se pamti za koj period je izvrsena, kad je predvidjena i kad se desila sl kupovina
+            throw new Exception();
+        }
+        public void addToLearningData()
+        {
+            // pozove se makeAllPredictions
+            // na osnovu tih predikcija treba napraviti vise SinglePointOfData
+            // svaki SPD se vezuje za jednu Predikciju
+            // ako je predikcija bila na osnovu perioda a-b
+            // u SPD se pravi statistika svih Predikcija koje su se desile do trenutka b
+            // treba ih svrstati u grupe do 2% do 5% do 10% i vise od 10% kako za taj item tako i za sve ostale
+            // na kraju se svi dodaju u LearningData
+            throw new Exception();
+        }
+    }
+}
---
+++ NN/Baza/DTO/Prediction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza.DTO
+{
+    public class Prediction
+    {
+        public string itemNo;
+        public DateTime from;
+        public DateTime to;
+        public DateTime predicted;
+        public DateTime occurred;
+        public int ordersInbetween;
+
+        public static Prediction makePrediction(string customer,string item,DateTime begin,DateTime end )
+        {
+            // na osnovu parametara potrebno je popuniti polja
+            // posmatra se period begin do end i predvidja se kad ce sledeca kupovina da bude
+            throw new Exception();
+        }
+    }
+}
