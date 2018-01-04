8bcb1ef51d4086fe5b5e5adba5fd03c2d0e995c0
nevena94rad <nevenarad1994@gmail.com>
2018-01-04 13:03:37 +0100

    prediction

--- NN/Baza/DTO/Prediction.cs
+++ NN/Baza/DTO/Prediction.cs
@@ -11,15 +11,26 @@ namespace Baza.DTO
         public string itemNo;
         public DateTime from;
         public DateTime to;
-        public DateTime predicted;
         public DateTime occurred;
+        public double predictedConsumption;
+        public int lastInvQty;
         public int ordersInbetween;
 
-        public static Prediction makePrediction(string customer,string item,DateTime begin,DateTime end )
+        public static Prediction makePrediction(string customer,string item,DateTime begin,DateTime end, DateTime nextPurchase, int lastInvQty)
         {
             // na osnovu parametara potrebno je popuniti polja
             // posmatra se period begin do end i predvidja se kad ce sledeca kupovina da bude
-            throw new Exception();
+            var db = new DataClasses1DataContext();
+
+            Prediction pred = new Prediction();
+            pred.itemNo = item;
+            pred.from = begin;
+            pred.to = end;
+            pred.occurred = nextPurchase;
+            pred.lastInvQty = lastInvQty;
+
+            string connectionString = db.Connection.ConnectionString;
+            
         }
     }
 }
