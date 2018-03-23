75678f9485243abb0b88d8d8de3232dc5b01f072
markko <marko.kovacevic@code3profit.com>
2018-03-23 10:31:19 +0100

    Drop and delete scripts added.

---
+++ Radna/Sql/script delete.sql
@@ -0,0 +1,10 @@
+delete from CustomerModel;
+GO
+delete from [Parameters];
+GO
+delete from PurchaseHistory;
+GO
+delete from PurchasePeriods;
+GO
+delete from PurchasePrediction;
+GO
---
+++ Radna/Sql/script drop.sql
@@ -0,0 +1,10 @@
+drop table [PurchasePrediction];
+GO
+drop table [PurchasePeriods];
+GO
+drop table [PurchaseHistory];
+GO
+drop table [CustomerModel];
+GO
+drop table [Parameters];
+GO
