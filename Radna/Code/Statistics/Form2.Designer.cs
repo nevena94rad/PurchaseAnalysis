87605a1eaed28f38bfd36bdb145db1dd2f7f0602
dev12 <dev12@profitoptics.com>
2018-03-28 14:07:01 +0200

    listBox

--- Radna/Code/Statistics/Form2.Designer.cs
+++ Radna/Code/Statistics/Form2.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.oldPredictions = new System.Windows.Forms.ListBox();
+            this.newPredictions = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.oldPredictions);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "OLD VERSION, correct predictions";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.newPredictions);
+            this.groupBox2.Location = new System.Drawing.Point(275, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(228, 426);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "NEW VERSION, correct predictions";
+            // 
+            // oldPredictions
+            // 
+            this.oldPredictions.FormattingEnabled = true;
+            this.oldPredictions.Location = new System.Drawing.Point(20, 36);
+            this.oldPredictions.Name = "oldPredictions";
+            this.oldPredictions.Size = new System.Drawing.Size(187, 355);
+            this.oldPredictions.TabIndex = 0;
+            // 
+            // newPredictions
+            // 
+            this.newPredictions.FormattingEnabled = true;
+            this.newPredictions.Location = new System.Drawing.Point(21, 36);
+            this.newPredictions.Name = "newPredictions";
+            this.newPredictions.Size = new System.Drawing.Size(187, 355);
+            this.newPredictions.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(515, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox oldPredictions;
+        private System.Windows.Forms.ListBox newPredictions;
     }
 }
--- Radna/Code/Statistics/Form2.cs
+++ Radna/Code/Statistics/Form2.cs
@@ -16,5 +16,10 @@ namespace Statistics
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
