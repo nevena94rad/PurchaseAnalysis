2e8cd888d283d16710135b1311a72ffd2e26603b
master.viper <master.viper@profitoptics.com>
2018-05-10 13:35:46 +0200

    I_ARIMAPrepare

--- Radna/Code/Baza/Prepare/I_ARIMAPrepare.cs
+++ Radna/Code/Baza/Prepare/I_ARIMAPrepare.cs
@@ -12,9 +12,9 @@ namespace Baza.Prepare
         ARIMAData ARIMAprepare(int date);
         List<string> ARIMAreadAllCustomers();
         List<ARIMAItemData> ARIMAreadAllItems(string custNo);
-        int ARIMAgetStartDate(string custNo, string itemNo);
-        int ARIMAgetEndDate(string custNo, string itemNo);
-        List<ARIMAConsumptionData> ARIMAgetCustomerConsumption(string custNo, string itemNo, int start, int end);
+        int ARIMAgetStartDate(string custNo, string itemNo, bool isGPI);
+        int ARIMAgetEndDate(string custNo, string itemNo, bool isGPI);
+        List<ARIMAConsumptionData> ARIMAgetCustomerConsumption(string custNo, string itemNo, int start, int end, bool isGPI);
         List<ARIMAConsumptionData> ARIMAgetGlobalConsumption(string itemNo, int start, int end);
         List<ARIMAConsumptionData> TransformQuantityData(List<ARIMAConsumptionData> quantity);
         string GetScriptPath();
